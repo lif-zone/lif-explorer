4d8dae78fae32b96183e24c9616eea42bcdade37
LIF Deploy <lifdeploy@lif.zone>
2026-08-22 01:48:00 +0300

    update

--- lang/index.js
+++ lang/index.js
@@ -17,8 +17,7 @@ import he from './he.json' with {type: 'json'};
 import jp from './jp.json' with {type: 'json'};
 import ko from './ko.json' with {type: 'json'};
 
-const langs = {
-  en, pt_pt, de, fr, it, es, nl, bg, ru, sr, hr, bs, me, sv, zh_ch, he, jp, ko};
-};
+const langs = {en, pt_pt, de, fr, it, es, nl, bg, ru, sr, hr, bs, me,
+  sv, zh_ch, he, jp, ko};
 
 export default langs;
