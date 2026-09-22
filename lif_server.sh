ba4cef4c4eef664436383e71fc520817c0764f7a
Bright Life <bright@bright.center>
2026-09-23 00:47:48 +0300

    update

--- lif_server.sh
+++ lif_server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 export PORT="1806"
 export API_URL="http://localhost:1842/.lif.net/blockstream/"
+#export PORT="1807"
+#export API_URL="https://blockstream.info/api/"
 
 babel-node dev-server.js
