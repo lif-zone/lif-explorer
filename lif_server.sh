fda9b5e2ae6acb9e5ec63f007f528fc107b13804
Bright Life <bright@bright.center>
2026-09-23 22:57:53 +0300

    update

--- lif_server.sh
+++ lif_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 export PORT=1806
 export API_URL=http://localhost:1842/.lif.net/blockstream/
+export NATIVE_ASSET_LABEL=LIF
 #export PORT=1807
 #export API_URL=https://blockstream.info/api/
 
